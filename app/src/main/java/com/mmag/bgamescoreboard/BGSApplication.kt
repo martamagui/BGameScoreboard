9369a0dfd52d9cc1f7917965ce79d0b2841fa5e2
martamagui <73697174+martamagui@users.noreply.github.com>
2025-02-26 16:03:37 +0100

    Added ANR detection

--- app/src/main/java/com/mmag/bgamescoreboard/BGSApplication.kt
+++ app/src/main/java/com/mmag/bgamescoreboard/BGSApplication.kt
@@ -1,6 +1,7 @@
 package com.mmag.bgamescoreboard
 
 import android.app.Application
+import android.os.StrictMode
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mmag.bgamescoreboard.data.db.BGSDatabase
@@ -9,4 +10,26 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BGSApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    //.penaltyDeath()
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedClosableObjects()
+                    .detectLeakedSqlLiteObjects()
+                    .penaltyLog()
+                    .build()
+            )
+        }
+    }
 }
